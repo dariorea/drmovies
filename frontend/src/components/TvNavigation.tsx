1631d200d6daf847cabe2aa6e1c85ff60ebab68d
dariorea <darioreaok@gmail.com>
2026-09-23 01:14:42 -0300

    dpad fixed

--- frontend/src/components/TvNavigation.tsx
+++ frontend/src/components/TvNavigation.tsx
@@ -1,34 +1,34 @@
-import { useEffect } from "react"
-import { useSpatialNavigation } from "@tv-spatial-navigation/react"
-
-export const TvNavigation = () => {
-
-    const navigation = useSpatialNavigation() as any
-
-    useEffect(() => {
-
-        const handleKeyDown = (event: KeyboardEvent) => {
-
-            const keys = [
-                "ArrowUp",
-                "ArrowDown",
-                "ArrowLeft",
-                "ArrowRight",
-                "Enter"
-            ]
-
-            if (!keys.includes(event.key)) return
-
-            navigation.handleKeyDown(event)
-        }
-
-        window.addEventListener("keydown", handleKeyDown)
-
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown)
-        }
-
-    }, [navigation])
-
-    return null
-}
+//import { useEffect } from "react"
+//import { useSpatialNavigation } from "@tv-spatial-navigation/react"
+//
+//export const TvNavigation = () => {
+//
+//    const navigation = useSpatialNavigation() as any
+//
+//    useEffect(() => {
+//
+//        const handleKeyDown = (event: KeyboardEvent) => {
+//
+//            const keys = [
+//                "ArrowUp",
+//                "ArrowDown",
+//                "ArrowLeft",
+//                "ArrowRight",
+//                "Enter"
+//            ]
+//
+//            if (!keys.includes(event.key)) return
+//
+//            navigation.handleKeyDown(event)
+//        }
+//
+//        window.addEventListener("keydown", handleKeyDown)
+//
+//        return () => {
+//            window.removeEventListener("keydown", handleKeyDown)
+//        }
+//
+//    }, [navigation])
+//
+//    return null
+//}
